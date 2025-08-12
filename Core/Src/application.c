59c704a4077719eb0448a9ba5cf15e3fc7bfb5be
oharakr <oharakr@gmail.com>
2025-08-12 14:29:58 -0300

    testando alterar arquivos

--- Core/Src/application.c
+++ Core/Src/application.c
@@ -1,7 +1,7 @@
 #include "cmsis_os.h"
 #include "stm32f4xx_hal.h"
 #include "radioenge_modem.h"
-
+//teste github
 extern osTimerId_t PeriodicSendTimerHandle;
 extern osThreadId_t AppSendTaskHandle;
 extern ADC_HandleTypeDef hadc1;
